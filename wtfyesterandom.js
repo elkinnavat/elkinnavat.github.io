fe1f02274fa4484e51a326f5f3227cf51615a900
Elkin Navarrete <90862238+elkinnavat@users.noreply.github.com>
2025-01-20 20:46:13 -0500

    separate js from html
    Eficiencia 5

---
+++ wtfyesterandom.js
@@ -0,0 +1,8 @@
+function toggleAudio() {
+    const audio = document.getElementById("wtfquehaceunmp3aqui");
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+    }
+}
