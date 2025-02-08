5fb5a6c97d990cf38810356f404a462438c279f4
Elkin Navarrete <90862238+elkinnavat@users.noreply.github.com>
2025-02-07 19:52:57 -0500

    cosas random con los idiomas

--- wtfyesterandom.js
+++ wtfyesterandom.js
@@ -19,9 +19,3 @@ function toggleAudio() {
 //BLOTWAREEEEEEE
 
 //idiomas 
-document.addEventListener("DOMContentLoaded", function() {
-    var userLang = navigator.language || navigator.userLanguage;
-    if (userLang.startsWith('en')) {
-        window.location.href = "index_en.html";
-    }
-});
